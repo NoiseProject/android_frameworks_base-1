1f9b272393630c2d6e01416e8431c34e51ede053
slukk <slukkica@gmail.com>
2016-09-13 10:20:01 +0200

    Ambient display: Battery status (1/2)
    - Show/hide battery status on ambient display
    
    rogersb11 Fix this with all our stuff
    Change KeyguardStatusView setup from view to allow control as doze isn't attached to "View"

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3745,6 +3745,11 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * @hide
+         */
+        public static final String AMBIENT_DISPLAY_SHOW_BATTERY = "ambient_display_show_battery";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
